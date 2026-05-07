83a24ee8785d4dbc9df602a327f7918435c019d4
J-Branch <jamalbranch01@gmail.com>
2026-05-07 13:40:05 -0400

    Initial spring boot project commit

---
+++ src/main/java/io/github/J_Branch/fit_log_api/FitLogApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.J_Branch.fit_log_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/J_Branch/fit_log_api/FitLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.J_Branch.fit_log_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
