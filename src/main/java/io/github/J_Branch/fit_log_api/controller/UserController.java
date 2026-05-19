5288f166789ced5e57658f833f35c8d22a5a9f51
J-Branch <jamalbranch01@gmail.com>
2026-05-19 11:36:09 -0400

    set up rest controller for users

---
+++ src/main/java/io/github/J_Branch/fit_log_api/controller/UserController.java
@@ -0,0 +1,33 @@
+package io.github.J_Branch.fit_log_api.controller;
+
+import io.github.J_Branch.fit_log_api.entity.User;
+import io.github.J_Branch.fit_log_api.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
